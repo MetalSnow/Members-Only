51abaadd8b25b1aab12adf01f857687ef57f6ac4
MetalSnow <aymanetrend@gmail.com>
2025-06-03 23:47:33 +0800

    feat: create initial files and install required packages

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('node:path');
+require('dotenv').config();
+
+const app = express();
+
+const assetspath = path.join(__dirname, '/public');
+
+app.use(express.static(assetspath));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.send('Welcome Member'));
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
